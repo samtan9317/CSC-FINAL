225ac87fc4b1741723a826d937bb851533e1c0e0
samta <samta@DESKTOP-24LTGBD>
2017-04-17 15:23:01 -0700

    Have fixed some minor errors and the three buttons are passed to controller.php by form submit.

--- controller.php
+++ controller.php
@@ -4,11 +4,23 @@
 //
 //
 
-include 'model.php';  // for $theDBA, an instance of DataBaseAdaptor
-include 'register.php';
-include 'login.php';
-include 'addQuote.php';
+//include 'model.php';  // for $theDBA, an instance of DataBaseAdaptor
+//include 'register.php';
+//include 'login.php';
+//include 'addQuote.php';
 
+print_r ($_POST['input']); //check if input value was passed correctly to php
+
+/*
+if($input == "Register" ){
+	echo $input;
+	header('location: register.php');
+}
+else if($input == "Login"){ // call login function and show Login interface
+	header('location: login.php');
+}
+else{// call add Quote function and show add Quote PHP page
+	header('location: addQuote.php');
+}*/
 //    Refresh/Update Original HTML
-echo  json_encode($arr);
 ?>
